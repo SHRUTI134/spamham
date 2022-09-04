08e2bce1775dc706525fe672d6120c54ce3a3578
SHRUTI134 <94781147+SHRUTI134@users.noreply.github.com>
2022-09-04 10:23:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ st.title("SPAM HAM CLASSIFICATION")
 ip = st.text_input("Enter your message:")
 
 #predict if the entered message is spam or ham 
+
+
+
+
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])  #prints the output as spam or ham  
